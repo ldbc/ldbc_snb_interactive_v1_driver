998987e2a983c792619aaa557a225f18e433a7bb
Alex Averbuch <alex.averbuch@gmail.com>
2013-06-20 11:08:37 +0200

    comment out old Neo4j connector so project compiles (it depends on old YCSB code)

--- neo4j/src/test/java/com/ldbc/driver/db/neo4j/Neo4jClientCommandsRestTest.java
+++ neo4j/src/test/java/com/ldbc/driver/db/neo4j/Neo4jClientCommandsRestTest.java
@@ -1,16 +1,16 @@
-package com.ldbc.driver.db.neo4j;
-
-import org.junit.Ignore;
-
-import com.ldbc.driver.db.neo4j.Neo4jClientCommands;
-import com.ldbc.driver.db.neo4j.Neo4jClientCommandsRest;
-
-@Ignore
-public class Neo4jClientCommandsRestTest extends Neo4jClientCommandsTest
-{
-    @Override
-    public Neo4jClientCommands getClientCommandsImpl()
-    {
-        return new Neo4jClientCommandsRest( "http://localhost:7474/db/data", PRIMARY_KEY );
-    }
-}
+//package com.ldbc.driver.db.neo4j;
+//
+//import org.junit.Ignore;
+//
+//import com.ldbc.driver.db.neo4j.Neo4jClientCommands;
+//import com.ldbc.driver.db.neo4j.Neo4jClientCommandsRest;
+//
+//@Ignore
+//public class Neo4jClientCommandsRestTest extends Neo4jClientCommandsTest
+//{
+//    @Override
+//    public Neo4jClientCommands getClientCommandsImpl()
+//    {
+//        return new Neo4jClientCommandsRest( "http://localhost:7474/db/data", PRIMARY_KEY );
+//    }
+//}
