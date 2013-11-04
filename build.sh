0c6f4847cd6e4e7082298fd193b3daf62e144905
Alex Averbuch <alex.averbuch@gmail.com>
2013-11-04 23:02:15 +0100

    update build.sh for new HdrHistogram version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HDR_HISTOGRAM="HdrHistogram"
-HDR_HISTOGRAM_JAR="HdrHistogram/target/hdrhistogram-1.0-SNAPSHOT.jar"
 HDR_HISTOGRAM_VER="1.0.2"
+HDR_HISTOGRAM_JAR="HdrHistogram/target/HdrHistogram-"${HDR_HISTOGRAM_VER}".jar"
 IN_PROJECT_MVN_REPO="core/lib"
 
 git submodule update --init
