0ef5320c36f6660d367cacb2b58dad5bac1d19a9
Alex Averbuch <alex.averbuch@gmail.com>
2015-10-27 14:19:22 +0100

    update limit of BI Q10 from 20 to 100

--- src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery10TagPerson.java
+++ src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery10TagPerson.java
@@ -9,8 +9,7 @@ import java.util.List;
 public class LdbcSnbBiQuery10TagPerson extends Operation<List<LdbcSnbBiQuery10TagPersonResult>>
 {
     public static final int TYPE = 10;
-    // TODO
-    public static final int DEFAULT_LIMIT = 20;
+    public static final int DEFAULT_LIMIT = 100;
     private final String tag;
     private final int limit;
 
