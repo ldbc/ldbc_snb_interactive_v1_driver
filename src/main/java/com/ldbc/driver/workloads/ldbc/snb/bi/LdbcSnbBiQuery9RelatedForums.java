b4e87a0aa8c1cffeaccd824856e8fe50be0c92ec
Alex Averbuch <alex.averbuch@gmail.com>
2015-10-27 12:34:36 +0100

    update BI Q9 limit from 20 to 100

--- src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery9RelatedForums.java
+++ src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery9RelatedForums.java
@@ -9,8 +9,7 @@ import java.util.List;
 public class LdbcSnbBiQuery9RelatedForums extends Operation<List<LdbcSnbBiQuery9RelatedForumsResult>>
 {
     public static final int TYPE = 9;
-    // TODO
-    public static final int DEFAULT_LIMIT = 20;
+    public static final int DEFAULT_LIMIT = 100;
     private final String tagClassA;
     private final String tagClassB;
     private final int threshold;
