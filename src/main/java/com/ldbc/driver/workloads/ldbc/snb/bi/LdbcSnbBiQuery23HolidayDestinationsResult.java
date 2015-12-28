4b292984521d1e6516f36d69405e50820d580a92
Alex Averbuch <alex.averbuch@gmail.com>
2015-12-28 11:58:02 +0100

    minor field renaming for BI Q23 Result

--- src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery23HolidayDestinationsResult.java
+++ src/main/java/com/ldbc/driver/workloads/ldbc/snb/bi/LdbcSnbBiQuery23HolidayDestinationsResult.java
@@ -2,20 +2,20 @@ package com.ldbc.driver.workloads.ldbc.snb.bi;
 
 public class LdbcSnbBiQuery23HolidayDestinationsResult
 {
-    private final String place;
+    private final String country;
     private final int month;
     private final int count;
 
-    public LdbcSnbBiQuery23HolidayDestinationsResult( String place, int month, int count )
+    public LdbcSnbBiQuery23HolidayDestinationsResult( String country, int month, int count )
     {
-        this.place = place;
+        this.country = country;
         this.month = month;
         this.count = count;
     }
 
     public String countryName()
     {
-        return place;
+        return country;
     }
 
     public int month()
@@ -31,8 +31,8 @@ public class LdbcSnbBiQuery23HolidayDestinationsResult
     @Override
     public String toString()
     {
-        return "LdbcSnbBiQuery23Result{" +
-               "place='" + place + '\'' +
+        return "LdbcSnbBiQuery23HolidayDestinationsResult{" +
+               "country='" + country + '\'' +
                ", month=" + month +
                ", count=" + count +
                '}';
@@ -52,14 +52,14 @@ public class LdbcSnbBiQuery23HolidayDestinationsResult
         { return false; }
         if ( count != that.count )
         { return false; }
-        return !(place != null ? !place.equals( that.place ) : that.place != null);
+        return country != null ? country.equals( that.country ) : that.country == null;
 
     }
 
     @Override
     public int hashCode()
     {
-        int result = place != null ? place.hashCode() : 0;
+        int result = country != null ? country.hashCode() : 0;
         result = 31 * result + month;
         result = 31 * result + count;
         return result;
