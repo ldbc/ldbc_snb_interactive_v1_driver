0404c843026f5fe0b47a33b0ad5219403b03eb26
Anil Pacaci <apacaci@uwaterloo.ca>
2017-03-19 18:19:33 -0400

    Read - Update reported seperately

--- src/main/java/com/ldbc/driver/runtime/metrics/WorkloadStatusSnapshot.java
+++ src/main/java/com/ldbc/driver/runtime/metrics/WorkloadStatusSnapshot.java
@@ -5,15 +5,16 @@ public class WorkloadStatusSnapshot {
     private final long operationCount;
     private final long durationSinceLastMeasurementAsMilli;
     private final double throughput;
+    private final long updateCount;
+    private final double updateThroughput;
 
-    public WorkloadStatusSnapshot(long runDurationAsMilli,
-                                  long operationCount,
-                                  long durationSinceLastMeasurementAsMilli,
-                                  double throughput) {
+    public WorkloadStatusSnapshot(long runDurationAsMilli, long operationCount, long durationSinceLastMeasurementAsMilli, double throughput, long updateCount, double updateThroughput) {
         this.runDurationAsMilli = runDurationAsMilli;
         this.operationCount = operationCount;
         this.durationSinceLastMeasurementAsMilli = durationSinceLastMeasurementAsMilli;
         this.throughput = throughput;
+        this.updateCount = updateCount;
+        this.updateThroughput = updateThroughput;
     }
 
     public long runDurationAsMilli() {
@@ -30,5 +31,13 @@ public class WorkloadStatusSnapshot {
 
     public double throughput() {
         return throughput;
+    }
+
+    public long getUpdateCount() {
+        return updateCount;
+    }
+
+    public double getUpdateThroughput() {
+        return updateThroughput;
     }
 }
