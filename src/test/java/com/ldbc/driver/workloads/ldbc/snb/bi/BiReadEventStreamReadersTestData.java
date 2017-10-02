4fa054a7be1e37b49e980feadb2ba9a6887e0735
Gabor Szarnyas <szarnyasg@gmail.com>
2017-10-02 14:03:16 -0400

    Rename attribute for query 12 input #77

--- src/test/java/com/ldbc/driver/workloads/ldbc/snb/bi/BiReadEventStreamReadersTestData.java
+++ src/test/java/com/ldbc/driver/workloads/ldbc/snb/bi/BiReadEventStreamReadersTestData.java
@@ -111,7 +111,7 @@ public class BiReadEventStreamReadersTestData
 
     public static final String QUERY_12_CSV_ROWS()
     {
-        return "creationDate|likeThreshold\n"
+        return "date|likeThreshold\n"
                + "1441351591755|1\n"
                + "1441351591756|2";
     }
