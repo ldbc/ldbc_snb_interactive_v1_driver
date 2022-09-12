806af9406808146495941fd947c4c77280e5fd82
David Puroja <dpuroja@outlook.com>
2022-09-12 14:12:24 +0200

    Remove duplicate queries

--- paramgen-queries/pg-13b.sql
+++
@@ -1,7 +0,0 @@
--- variant (b): guaranteed that there is a path
-SELECT
-    person1Id AS 'person1Id',
-    person2Id AS 'person2Id'
-FROM people4Hops
-ORDER BY md5(concat(person1Id + 1, person2Id + 2))
-LIMIT 500
