21856e15fa9ceea697057df2770818747fe6cad4
Gabor Szarnyas <szarnyasg@gmail.com>
2022-10-18 18:20:09 +0200

    Conversion script: Update comment

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -8,5 +8,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf {deletes,inserts}
 mkdir {deletes,inserts}
 
-echo "##### Add dependencyTime column #####"
+echo "##### Generates insert and delete streams, adds dependencyTime column #####"
 ./convert_and_append.py --input_dir ${LDBC_SNB_DATA_ROOT_DIRECTORY} --output_dir ./ --input_type ${DATA_INPUT_TYPE} --data_format composite-merged-fk
