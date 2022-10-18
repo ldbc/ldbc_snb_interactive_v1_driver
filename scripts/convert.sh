840b280c37b7cdad934a6c645dbc57fe300925e0
Gabor Szarnyas <szarnyasg@gmail.com>
2022-10-18 18:18:52 +0200

    Conversion script: Remove commented code

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -8,8 +8,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf {deletes,inserts}
 mkdir {deletes,inserts}
 
-# echo "##### Convert Datagen data sets #####"
-# ./convert_spark_dataset_to_interactive.py --input_dir ${LDBC_SNB_DATA_ROOT_DIRECTORY} --output_dir ./
-
 echo "##### Add dependencyTime column #####"
 ./convert_and_append.py --input_dir ${LDBC_SNB_DATA_ROOT_DIRECTORY} --output_dir ./ --input_type ${DATA_INPUT_TYPE} --data_format composite-merged-fk
