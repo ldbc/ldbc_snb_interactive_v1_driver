e40401efe15c13ded1bfbde4f48152f142178d1b
Gabor Szarnyas <szarnyasg@gmail.com>
2022-10-31 22:10:05 +0100

    Paramgen: Fix paths for sample data, make download script more robust

--- paramgen/scripts/get-sample-factors.sh
+++ paramgen/scripts/get-sample-factors.sh
@@ -6,6 +6,8 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+rm -f *.zip
+rm -rf social-network-sf0.003*/
 rm -rf scratch/factors
 mkdir -p scratch/factors
 wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-factors.zip
@@ -16,4 +18,5 @@ mkdir -p scratch/sample-data
 wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-merged-fk.zip
 unzip -q social-network-sf0.003-bi-composite-merged-fk.zip
 
-cp -r social-network-sf0.003-bi-factors/factors/parquet/raw/composite-merged-fk/* scratch/factors/
+mv social-network-sf0.003-bi-factors/factors/parquet/raw/composite-merged-fk/* scratch/factors/
+mv social-network-sf0.003-bi-composite-merged-fk scratch/sample-data/
--- paramgen/scripts/use-sample-factors.sh
+++ paramgen/scripts/use-sample-factors.sh
@@ -1,1 +1,1 @@
-export LDBC_SNB_DATA_ROOT_DIRECTORY=scratch/sample-data/social-network-sf0.003-bi-factors/factors/parquet/raw
+export LDBC_SNB_DATA_ROOT_DIRECTORY=scratch/sample-data/social-network-sf0.003-bi-composite-merged-fk/graphs/parquet/raw
