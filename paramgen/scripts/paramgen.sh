90902d9a69d0596aa07c97f3bad36d529cc75774
David Puroja <dpuroja@outlook.com>
2022-11-08 00:31:03 +0100

    Concat consecutive days

--- paramgen/scripts/paramgen.sh
+++ paramgen/scripts/paramgen.sh
@@ -6,4 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-python3 paramgen.py --raw_parquet_dir ${LDBC_SNB_DATA_ROOT_DIRECTORY} --factor_tables_dir 'scratch/factors/*' --start_date '2012-11-28' --end_date '2013-01-01' --time_bucket_size_in_days 1 --generate_short_query_parameters True
+python3 paramgen.py --raw_parquet_dir ${LDBC_SNB_DATA_ROOT_DIRECTORY} --factor_tables_dir 'scratch/factors/' --time_bucket_size_in_days 1 --generate_short_query_parameters True
