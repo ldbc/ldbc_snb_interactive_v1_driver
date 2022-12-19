d4df218d50093cae21b6bb7283b1cb8698789a32
Gabor Szarnyas <szarnyasg@gmail.com>
2022-12-19 13:31:52 +0100

    Add helper scripts

---
+++ scripts/package-mvn-artifacts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+git clean -xdf .
+mvn clean deploy
+cp -r target/snb-mvn/* ../snb-mvn/
