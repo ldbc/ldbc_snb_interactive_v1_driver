8362f4df8db379edebc3dca606025a4007fb2ad7
Gabor Szarnyas <szarnyasg@gmail.com>
2023-01-04 07:52:00 +0100

    Paramgen: Force passing the factor directory to the paramgen as argument

--- paramgen/paramgen.py
+++ paramgen/paramgen.py
@@ -109,10 +109,10 @@ class ParameterGeneration():
         path_curation = PathCuration(self.raw_parquet_dir, self.factor_tables_dir)
         path_curation.get_people_4_hops_paths(self.start_date, self.end_date, 1, parquet_output_dir)
         self.logger.debug("============ Done ============")
-        files = glob.glob('scratch/factors/people4Hops/*')
+        files = glob.glob(f"{self.factor_tables_dir}/people4Hops/*")
         for f in files:
             print(f)
-            if f != 'scratch/factors/people4Hops/curated_paths.parquet':
+            if f != f"{self.factor_tables_dir}/people4Hops/curated_paths.parquet":
                 os.remove(f)
         self.create_views_of_factor_tables(self.factor_tables_dir)
 
@@ -245,10 +245,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         '--factor_tables_dir',
-        help="factor_tables_dir: directory containing the factor tables e.g. '/data/out-sf1'",
+        help="factor_tables_dir: directory containing the factor tables e.g. '/data/out-sf1/factors/parquet/raw/composite-merged-fk/'",
         type=str,
-        default='factors/',
-        required=False
+        required=True
     )
     parser.add_argument(
         '--time_bucket_size_in_days',
--- paramgen/scripts/get-sample-data.sh
+++ paramgen/scripts/get-sample-data.sh
@@ -4,19 +4,18 @@ set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ..
+cd ../scratch
 
 rm -f *.zip
 rm -rf social-network-sf0.003*/
-rm -rf scratch/factors
-mkdir -p scratch/factors
+rm -rf sample-data
+mkdir -p sample-data
+
 wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-factors.zip
 unzip -q social-network-sf0.003-bi-factors.zip
 
-rm -rf scratch/sample-data
-mkdir -p scratch/sample-data
 wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-merged-fk.zip
 unzip -q social-network-sf0.003-bi-composite-merged-fk.zip
 
-mv social-network-sf0.003-bi-factors/factors/parquet/raw/composite-merged-fk/* scratch/factors/
-mv social-network-sf0.003-bi-composite-merged-fk scratch/sample-data/
+mv social-network-sf0.003-bi-factors sample-data/
+mv social-network-sf0.003-bi-composite-merged-fk sample-data/
